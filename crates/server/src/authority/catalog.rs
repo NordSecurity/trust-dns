d625d65070db994b6489f33973475b21c6be1a90
Mathias Peters <mathias.peters@nordsec.com>
2025-10-28 00:54:00 +0100

    test

--- crates/server/src/authority/catalog.rs
+++ crates/server/src/authority/catalog.rs
@@ -781,7 +781,7 @@ async fn build_forwarded_response(
         }
         Ok(l) => (Answer::Normal(l), Box::<AuthLookup>::default()),
         Err(e) if e.is_no_records_found() || e.is_nx_domain() => {
-            debug!(error = ?e, "error resolving");
+            debug!(error = ?e, "error resolving 1");
 
             if e.is_nx_domain() {
                 response_header.set_response_code(ResponseCode::NXDomain);
@@ -833,7 +833,7 @@ async fn build_forwarded_response(
             }
         }
         Err(e) => {
-            debug!(error = ?e, "error resolving");
+            debug!(error = ?e, "error resolving 2");
             return Err(e);
         }
     };
